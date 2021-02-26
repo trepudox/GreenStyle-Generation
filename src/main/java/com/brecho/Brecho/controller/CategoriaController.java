76ea38b78d7bdf1c69ae7a3687c3f1f12d6ee03a
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-02-26 14:14:09 -0300

    Injeção de depend.

--- src/main/java/com/brecho/Brecho/controller/CategoriaController.java
+++ src/main/java/com/brecho/Brecho/controller/CategoriaController.java
@@ -1,8 +1,14 @@
 package com.brecho.Brecho.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brecho.Brecho.repository.CategoriaRepository;
 
 @RestController
 public class CategoriaController {
-
+	@Autowired
+	private CategoriaRepository repository;
+	
 }
