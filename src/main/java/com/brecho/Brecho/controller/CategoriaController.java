b3774f7e00ce17ae7911a4e887a46535d99dbbee
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-02-26 14:57:20 -0300

    RequestMapping adicionado

--- src/main/java/com/brecho/Brecho/controller/CategoriaController.java
+++ src/main/java/com/brecho/Brecho/controller/CategoriaController.java
@@ -2,12 +2,15 @@ package com.brecho.Brecho.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brecho.Brecho.repository.CategoriaRepository;
 
 @RestController
+@RequestMapping(path = "/categoria")
 public class CategoriaController {
+	
 	@Autowired
 	private CategoriaRepository repository;
 	
