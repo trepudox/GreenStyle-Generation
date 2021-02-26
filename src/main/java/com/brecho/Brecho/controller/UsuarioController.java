c74a8d3f00029995fc6911db0f5dc07006eb8064
Raquel Borges <raquel.olborges@gmail.com>
2021-02-26 14:58:52 -0300

    autowired usuario

--- src/main/java/com/brecho/Brecho/controller/UsuarioController.java
+++ src/main/java/com/brecho/Brecho/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.brecho.Brecho.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,4 +12,6 @@ import com.brecho.Brecho.repository.UsuarioRepository;
 @CrossOrigin(origins="*", allowedHeaders = "*")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioRepository repository;
 }
