e239a2d30481470108b73c7d8574e894132a7865
Raquel Borges <raquel.olborges@gmail.com>
2021-02-26 14:53:20 -0300

    autowired

--- src/main/java/com/brecho/Brecho/controller/ProdutoController.java
+++ src/main/java/com/brecho/Brecho/controller/ProdutoController.java
@@ -1,8 +1,14 @@
 package com.brecho.Brecho.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brecho.Brecho.repository.ProdutoRepository;
 
 @RestController
 public class ProdutoController {
 
+	@Autowired
+	private ProdutoRepository repository;
+	
 }
