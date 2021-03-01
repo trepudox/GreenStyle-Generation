53262d46b0a40686186b451267e0291fbe1b7c99
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-03-01 12:50:48 -0300

    Métodos Get adicionados

--- src/main/java/com/brecho/Brecho/controller/ProdutoController.java
+++ src/main/java/com/brecho/Brecho/controller/ProdutoController.java
@@ -1,5 +1,7 @@
 package com.brecho.Brecho.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +27,30 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repository;
 	
+	@GetMapping
+	public ResponseEntity<List<Produto>> findAll() {
+		return repository.findAll().size() == 0 ? ResponseEntity.status(HttpStatus.NO_CONTENT).build() : 
+			ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> findById(@PathVariable(name="id") Long id) {
+		return repository.findById(id).isEmpty() ? ResponseEntity.status(HttpStatus.NO_CONTENT).build() : 
+			ResponseEntity.status(HttpStatus.OK).body(repository.findById(id).get());
+	}
+	
+	@GetMapping("/nomeProduto/{nome}")
+	public ResponseEntity<List<Produto>> findByNomeContaining(@PathVariable(name="nome") String nome) {
+		return repository.findByNomeContainingIgnoreCase(nome).size() == 0 ? ResponseEntity.status(HttpStatus.NO_CONTENT).build() : 
+			ResponseEntity.status(HttpStatus.OK).body(repository.findByNomeContainingIgnoreCase(nome));
+	}
+	
+	@GetMapping("/disponivel")
+	public ResponseEntity<List<Produto>> findByDisponivel() {
+		return repository.findByDisponivel().size() == 0 ? ResponseEntity.status(HttpStatus.NO_CONTENT).build() : 
+			ResponseEntity.status(HttpStatus.OK).body(repository.findByDisponivel());
+	}
+
 	@PostMapping
 	public ResponseEntity<Produto> post (@RequestBody Produto produto){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
@@ -37,4 +65,6 @@ public class ProdutoController {
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
 	}
+
+	
 }
