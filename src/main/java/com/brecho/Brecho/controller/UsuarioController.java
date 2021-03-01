3fee95c73d96c5084a20b566b21dc09de7a1d9c0
Raquel Borges <raquel.olborges@gmail.com>
2021-03-01 11:21:12 -0300

    controller Usuario

--- src/main/java/com/brecho/Brecho/controller/UsuarioController.java
+++ src/main/java/com/brecho/Brecho/controller/UsuarioController.java
@@ -1,10 +1,21 @@
 package com.brecho.Brecho.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brecho.Brecho.model.Usuario;
 import com.brecho.Brecho.repository.UsuarioRepository;
 
 @RestController
@@ -14,4 +25,37 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> findAll ()
+	{
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping ("/{id}")
+	public ResponseEntity<Usuario> findById (@PathVariable Long id)
+	{
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> postUsuario (@RequestBody Usuario usuario)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> putUsuario (@RequestBody Usuario usuario)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuario));
+	}
+	
+	@DeleteMapping ("/{id}")
+	public void deleteUsuario (@PathVariable Long id)
+	{
+		repository.deleteById(id);
+	}
+	
+	
 }
