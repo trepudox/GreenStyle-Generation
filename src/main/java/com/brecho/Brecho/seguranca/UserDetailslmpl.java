4a6961244eaee6e6ca65849818649a7271a999c2
morenopep <pedrowarsps4@gmail.com>
2021-03-04 14:18:47 -0300

    adicionando a Classe UserDetailslmpl

---
+++ src/main/java/com/brecho/Brecho/seguranca/UserDetailslmpl.java
@@ -0,0 +1,64 @@
+package com.brecho.Brecho.seguranca;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.brecho.Brecho.model.Usuario;
+
+public class UserDetailslmpl implements UserDetails {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	
+	private String password;
+	
+	private List<GrantedAuthority> authorities;
+	
+	public UserDetailslmpl() {}
+	
+	public UserDetailslmpl(Usuario user) {
+		this.username = user.getNome();
+		this.password = user.getSenha();
+	} 
+	
+	@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			return authorities;
+		}
+
+	@Override
+		public String getPassword() {
+			return password;
+		}
+
+	@Override
+		public String getUsername() {
+			return username;
+		}
+
+	@Override
+		public boolean isAccountNonExpired() {
+			return true;
+		}
+
+	@Override
+		public boolean isAccountNonLocked() {
+			return true;
+		}
+
+	@Override
+		public boolean isCredentialsNonExpired() {
+			return true;
+		}
+
+	@Override
+		public boolean isEnabled() {
+			return true;
+		}
+
+	
+}
