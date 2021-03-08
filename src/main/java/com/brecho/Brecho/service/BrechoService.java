4ee41b0f90249ce5fa3dce6a1f46618694eb8163
Pedro Estevão <pedrowarsps4@gmail.com>
2021-03-08 15:25:56 -0300

    add Atualizações de post put e delete no brecho

--- src/main/java/com/brecho/Brecho/service/BrechoService.java
+++ src/main/java/com/brecho/Brecho/service/BrechoService.java
@@ -3,7 +3,11 @@ package com.brecho.Brecho.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.brecho.Brecho.model.Brecho;
 import com.brecho.Brecho.repository.BrechoRepository;
@@ -27,4 +31,19 @@ public class BrechoService {
 	public List<Brecho> findByName(String nome){
 		return repository.findAllByNomeContainingIgnoreCase(nome);
 	}
+	
+	public Brecho postBrecho (Brecho brecho){
+		return repository.save(brecho);
+	}		
+	
+	public ResponseEntity<Brecho> putBrecho(@RequestBody Brecho brecho){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(brecho));	
+	}		
+
+	public void deleteById(@PathVariable Long id) {
+		repository.findById(id).map( resp -> {
+			repository.delete(resp);
+			return Void.TYPE;
+			});
+	}	
 }
