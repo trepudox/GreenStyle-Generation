55392ea5522f833b32dc92b6cbf59e718eb46500
Gustavo Vilela <gutusmao12@hotmail.com>
2021-03-26 17:49:40 -0300

    Subindo navbar

---
+++ Front-End/greenStyle/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { HomeComponent } from './componentes/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RodapeComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
