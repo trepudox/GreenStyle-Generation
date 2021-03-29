7c0cdd74372fe1f835b0b8109033ee6db26402f3
Marco Aurélio <teoaa2@gmail.com>
2021-03-29 15:56:32 -0300

    criação dos novos componentes

--- Front-End/greenStyle/src/app/app.module.ts
+++ Front-End/greenStyle/src/app/app.module.ts
@@ -7,13 +7,25 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { ListaBrechosComponent } from './componentes/lista-brechos/lista-brechos.component';
+import { SolicitacaoParceiroComponent } from './componentes/solicitacao-parceiro/solicitacao-parceiro.component';
+import { ListaProdutosGeralComponent } from './componentes/lista-produtos-geral/lista-produtos-geral.component';
+import { ListaProdutosBrechoComponent } from './componentes/lista-produtos-brecho/lista-produtos-brecho.component';
+import { SobreNosComponent } from './componentes/sobre-nos/sobre-nos.component';
+import { CarrinhoComponent } from './componentes/carrinho/carrinho.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     RodapeComponent,
-    HomeComponent
+    HomeComponent,
+    ListaBrechosComponent,
+    SolicitacaoParceiroComponent,
+    ListaProdutosGeralComponent,
+    ListaProdutosBrechoComponent,
+    SobreNosComponent,
+    CarrinhoComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front-End/greenStyle/src/app/componentes/solicitacao-parceiro/solicitacao-parceiro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitacao-parceiro',
+  templateUrl: './solicitacao-parceiro.component.html',
+  styleUrls: ['./solicitacao-parceiro.component.css']
+})
+export class SolicitacaoParceiroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
