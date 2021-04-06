f881bfc1127dbea63b7c230ed2f4efa4cf11946f
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-04-06 14:06:20 -0300

    Metodos HTTP | Produtos

--- Front-End/greenStyle/src/app/service/produto.service.ts
+++ Front-End/greenStyle/src/app/service/produto.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Produto } from '../Models/Produto';
@@ -12,16 +12,37 @@ export class ProdutoService {
     private httpClient: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token )
+  }
 
   getAllProdutos(): Observable<Produto[]>{
     return this.httpClient.get<Produto[]>('http://localhost:8080/produto')
   }
 
-  getByIdProdutos(id: number): Observable<Produto>{
+  getByIdProduto(id: number): Observable<Produto>{
     return this.httpClient.get<Produto>(`http://localhost:8080/produto/${id}`)
+  }
+
+  getByNomeProduto(nome: string): Observable<Produto[]>{
+    return this.httpClient.get<Produto[]>(`http://localhost:8080/produto/nomeProduto/${nome}`)
   }
 
   getByIdBrechoProdutos(id: number): Observable<Produto[]>{
     return this.httpClient.get<Produto[]>(`http://localhost:8080/produto/brecho/${id}`)
   }
+
+  postProduto(produto:Produto): Observable<Produto>{
+    return this.httpClient.post<Produto>('http://localhost:8080/produto', produto, this.token)
+  }
+
+  putProduto(produto:Produto): Observable<Produto>{
+    return this.httpClient.put<Produto>('http://localhost:8080/produto', produto, this.token)
+  }
+
+  deleteTema(id:number){
+    return this.httpClient.delete (`http://localhost:8080/produto/brecho/${id}`, this.token)
+  }
+
+
 }
