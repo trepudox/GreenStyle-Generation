36df8c00b33309863fab3363e52601ba7cd2deda
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-04-06 13:33:58 -0300

    lista de brechos fazendo consumo corretamente, necessário apenas alterar o método de redirecionamento

--- Front-End/greenStyle/src/app/componentes/lista-brechos/lista-brechos.component.ts
+++ Front-End/greenStyle/src/app/componentes/lista-brechos/lista-brechos.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Brecho } from 'src/app/Models/Brecho';
+import { BrechoService } from 'src/app/service/brecho.service';
+import { environment } from "src/environments/environment.prod"
 
 @Component({
   selector: 'app-lista-brechos',
@@ -8,11 +12,29 @@ import { Router } from '@angular/router';
 })
 export class ListaBrechosComponent implements OnInit {
 
-  constructor(private router: Router,
-     ) { }
+  listaBrecho: Brecho[]
 
-  ngOnInit(){
+  constructor(
+    private router: Router,
+    private brechoService: BrechoService
+  ) { }
+
+  ngOnInit() {
     window.scroll(0, 0)
+
+    this.setListaBrecho()
+
+  }
+
+  setListaBrecho() {
+    this.brechoService.getAll().subscribe((resp: Brecho[]) => {
+      this.listaBrecho = resp
+    })
+  }
+
+  irParaBrecho(id: number) {
+    console.log(id)
+    this.router.navigate(["/home"])
   }
 
 }
