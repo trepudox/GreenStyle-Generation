29b976de67b75b9ef73d62ace3ab1f96c4054313
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-04-06 13:35:29 -0300

    Model e service de brechó criados, app module alterado com inserção do HttpClientModule

--- Front-End/greenStyle/src/app/service/brecho.service.ts
+++ Front-End/greenStyle/src/app/service/brecho.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http"
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment.prod";
+import { Brecho } from "../Models/Brecho";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrechoService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set("Authorization", environment.token)
+  }
+
+  getAll(): Observable<Brecho[]> {
+    return this.http.get<Brecho[]>("http://localhost:8080/brecho", this.token)
+  }
+
+  getById(id: number): Observable<Brecho> {
+    return this.http.get<Brecho>(`http://localhost:8080/brecho/${id}`, this.token)
+  }
+
+  getByNome(nome: string): Observable<Brecho[]> {
+    return this.http.get<Brecho[]>(`http://localhost:8080/nomeBrecho/${nome}`, this.token)
+  }
+
+  post(brecho: Brecho): Observable<Brecho> {
+    return this.http.post<Brecho>("http://localhost:8080/brecho", brecho, this.token)
+  }
+
+  put(brecho: Brecho): Observable<Brecho> {
+    return this.http.put<Brecho>("http://localhost:8080/brecho", brecho, this.token)
+  }
+
+  deleteBrecho(id: number) {
+    return this.http.delete(`http://localhost:8080/brecho/${id}`, this.token)
+  }
+
 }
